9cff6ba6d1418ca69d7a81c54edbfdc6eeb6135d
ildar <yunussov@yahoo.com>
2023-08-16 23:26:30 -0700

    Added a new File AppiumTest2.java

---
+++ src/test/java/qaclickacademy/AppiumTest 2.java
@@ -0,0 +1,36 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void NativeAPpAndroid()
+	{
+		System.out.println("NativeAPpAndroid");
+		
+		System.out.println("NativeAPpAndroid");
+	}
+	@Test
+	public void NativeAPpAndroidS()
+	{
+		System.out.println("NativeAPpAndroid");
+		
+		System.out.println("NativeAPpAndroid");
+	}
+	
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOSApps");	
+		
+		System.out.println("IOSApps");	
+		System.out.println("IOSApps");
+		
+		System.out.println("DEVELOP BRANCH ILDAR");	
+		System.out.println("DEVELOP BRANCH ILDAR");	
+		System.out.println("DEVELOP BRANCH ILDAR");	
+		System.out.println("DEVELOP BRANCH ILDAR");	
+		
+		
+	}
+}
